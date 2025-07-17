c011983d614def76b91464539c02a0cf2f056dfd
Hari vardhana shubhash <Hari.Ponnamanda@valuemomentum.com>
2025-07-17 11:08:21 +0530

    added all prompts. dynamic prompts are defines as functions the return fstring and static prompts ad multi-lined string variable

---
+++ src/helpers/prompts.py
@@ -0,0 +1,40 @@
+def vehicle_validator_prompt(vehicle_details: list) -> str:
+    return f"""You are a vehicle validation expert.
+
+You will be provided with images of a vehicle taken from different angles. Your task is to validate the vehicle based on the following criteria:
+1. Vehicle Make
+2. Vehicle Model
+3. Vehicle Year
+
+Based on the images and the provided vehicle details, respond in one of the following ways:
+
+- If the images and details are **insufficient** to make a judgment, return:
+  "Insufficient data"
+
+- If the vehicle is clearly **not valid**, return:
+  "Not valid"
+
+- If the vehicle is **valid**, return:
+  "Valid" along with the following JSON structure:
+    "data": "sufficient",
+    "validity": "valid"
+
+Here are the vehicle details:
+{vehicle_details}
+"""
+
+vehicle_details_extract_prompt = """You are an expert in vehicle detail extraction.
+Your task is to analyze images of a vehicle taken from different angles and extract the following details:
+1. Vehicle Make
+2. Vehicle Model
+3. Manufacturing Year (must be between 2000 and 2023)
+4. Visible damages report in the images
+Respond only with a JSON object in the following format:
+```json
+{{
+  "make": "Vehicle Make",
+  "model": "Vehicle Model",
+  "Manufacturing_year_range": "Vehicle Year"
+  "damages": "Description of visible damages"
+}}"""
+
