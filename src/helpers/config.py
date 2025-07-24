da54f0c904c4f1b84b1b7c1cf28ab0ffde3cc1f4
Hari vardhana shubhash <Hari.Ponnamanda@valuemomentum.com>
2025-07-24 19:16:15 +0530

    removed exposed api

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -1,9 +1,11 @@
 import os
 
-api= "AIzaSyB54b40k_aL-vsn0qhGQaVG1b9Gbl2IKeE"
+api= os.environ["gemini_API"]
 
 vehicle_images_path =r"C:\practice\challenge\data\images"
 
 database_path = "C:/practice/challenge/data/user/policy.db"
 
 SQLALCHEMY_DATABASE_URL = f"sqlite:///{database_path}"
+
+print(api)
