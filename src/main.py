8d9561eef3a9b24c4354791f726f4bd83d6423ed
Hari vardhana shubhash <Hari.Ponnamanda@valuemomentum.com>
2025-07-24 18:55:42 +0530

    includet the routes in the app [ from routers folder ]

--- src/main.py
+++ src/main.py
@@ -1,7 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI   # type: ignore
 import database.model as model
 from database.database import engine
+from routers import auth, policy, vehicle, user, llmRoute
 
 app = FastAPI()
 
-model.base.metadata.create_all(bind=engine) # type: ignore #ignore
+model.base.metadata.create_all(bind=engine) # type: ignore 
+
+
+app.include_router(user.router)
+
+app.include_router(policy.router)
+
+app.include_router(vehicle.router)
+
+app.include_router(llmRoute.router)
+
+app.include_router(auth.router)
+
+
+
+
+
+
+
+
+
---
+++ src/screens/home.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from screens.policies import new_policy_page
+import streamlit as st
+import datetime
+import os
+import base64
+from database.tables import Policy, Vehicle
+import streamlit.components.v1 as components
+
+
+def home_page():
+    pass
+
+home_page()
