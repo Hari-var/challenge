42909d83692edd616f9aee034c2525749d6cf04a
Hari vardhana shubhash <Hari.Ponnamanda@valuemomentum.com>
2025-07-24 19:00:35 +0530

    removed all the sqlite3 functionality and moved to endpoints and added Claims schema in model.py

--- src/database/model.py
+++ src/database/model.py
@@ -1,22 +1,27 @@
 from database.database import base
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Float
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Float, CheckConstraint
 from sqlalchemy.orm import relationship
 
 class User(base):
     __tablename__ = "users"
 
     user_id = Column(Integer, primary_key=True, autoincrement=True)
-    usertype = Column(String, nullable=False, default='user', check_constraint="usertype IN ('user', 'agent', 'admin')")
+    usertype = Column(String, nullable=False, default='user')
     username = Column(String, unique=True, nullable=False)
     firstname = Column(String, nullable=False)
     middlename = Column(String)
     lastname = Column(String, nullable=False)
-    password = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
     dateofbirth = Column(DateTime, nullable=False)
     phone = Column(String)
     email = Column(String, unique=True, nullable=False)
     address = Column(String)
+
     policies = relationship("Policy", back_populates="user")
+
+    __table_args__ = (
+        CheckConstraint("usertype IN ('user', 'agent', 'admin')", name="user_type_check"),
+    )
 
 class Policy(base):
     __tablename__ = "policies"
@@ -24,25 +29,59 @@ class Policy(base):
     policy_id = Column(Integer, primary_key=True, autoincrement=True)
     policy_number = Column(String, unique=True, nullable=False)
     policy_holder = Column(String, nullable=False)
-    user_id = Column(Integer, ForeignKey("users.user_id"))
+    user_id = Column(Integer, ForeignKey("users.user_id", ondelete="CASCADE"))
     start_date = Column(String, nullable=False)
     end_date = Column(String, nullable=False)
     premium = Column(Float, nullable=False)
+    total_claimable_amount = Column(Float, nullable=False)
     status = Column(String, nullable=False)
 
     user = relationship("User", back_populates="policies")
     vehicles = relationship("Vehicle", back_populates="policy")
+    claims = relationship("Claims", back_populates="policy")
+
+    __table_args__ = (
+        CheckConstraint("status IN ('active', 'inactive', 'expired')", name="policy_status_check"),
+    )
 
 class Vehicle(base):
     __tablename__ = "vehicles"
 
     vehicle_id = Column(Integer, primary_key=True, autoincrement=True)
-    policy_id = Column(Integer, ForeignKey("policies.policy_id"), nullable=False)
+    policy_id = Column(Integer, ForeignKey("policies.policy_id", ondelete="CASCADE"), nullable=False)
     typeofvehicle = Column(String, nullable=False)
     image_path = Column(String)
     make = Column(String, nullable=False)
     model = Column(String, nullable=False)
     year_of_purchase = Column(Integer, nullable=False)
+    chasis_no = Column(String, nullable=False)
+    vehicle_no= Column(String, nullable=False)
     damage_report = Column(String)
 
     policy = relationship("Policy", back_populates="vehicles")
+    claims = relationship("Claims", back_populates="vehicle")
+
+class Claims(base):
+    __tablename__ = "claims"
+
+    claim_id = Column(Integer, primary_key=True, autoincrement=True)
+    policy_id = Column(Integer, ForeignKey("policies.policy_id"), nullable=False)
+    vehicle_id = Column(Integer, ForeignKey("vehicles.vehicle_id"), nullable=False)
+    claim_number = Column(String, unique=True, nullable=False)
+    damage_description = Column(String, nullable=False)
+    damage_percentage = Column(Float, nullable=False)
+    damage_image_path = Column(String)
+    date_of_incident = Column(DateTime, nullable=False)
+    location_of_incident = Column(String, nullable=False)
+    fir_no = Column(String, nullable=True) 
+    claim_date = Column(DateTime, nullable=True)
+    requested_amount = Column(Float, nullable=False)
+    approved_amount = Column(Float, nullable=True)
+    claim_status = Column(String, nullable=False)
+
+    policy = relationship("Policy", back_populates="claims")
+    vehicle = relationship("Vehicle", back_populates="claims")
+
+    __table_args__ = (
+        CheckConstraint("claim_status IN ('active', 'inactive', 'expired')", name="claim_status_check"),
+    )
--- src/database/tables.py
+++ src/database/tables.py
@@ -1,272 +1,63 @@
 import sqlite3
 import os
 from datetime import datetime
-from helpers.config import database_path
-from abc import ABC, abstractmethod
+# from helpers.config import database_path
+from typing import Annotated
+from database.database import get_db, sessionlocal
+from database.model import User, Policy, Vehicle, Claims
+# from abc import ABC, abstractmethod
 
-#constant DB path
-DB_PATH = database_path
+# #constant DB path
+# DB_PATH = database_path
 
 
-class Database(ABC):
-    # @abstractmethod
-    # def initialize_DB(self, db_path=DB_PATH):
-    #     pass
+# class Database(ABC):
+#     # @abstractmethod
+#     # def initialize_DB(self, db_path=DB_PATH):
+#     #     pass
 
-    @abstractmethod
-    def get_by_id(self, id,db_path=DB_PATH):
-        pass
+#     @abstractmethod
+#     def get_by_id(self, id,db_path=DB_PATH):
+#         pass
 
-    @abstractmethod
-    def add(self, **kwargs):
-        pass
+#     @abstractmethod
+#     def add(self, **kwargs):
+#         pass
 
-    @abstractmethod
-    def update(self, **kwargs):
-        pass
+#     @abstractmethod
+#     def update(self, **kwargs):
+#         pass
 
-    @abstractmethod
-    def get_all(self, db_path=DB_PATH) -> list:
-        pass
+#     @abstractmethod
+#     def get_all(self, db_path=DB_PATH) -> list:
+#         pass
 
-class User(Database):
+class user_methods():
 
-    def get_by_id(self, id, db_path=DB_PATH):
-        conn = sqlite3.connect(db_path)
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM users WHERE user_id = ?", (id,))
-        result = cursor.fetchone()
-        conn.close()
-        return result
-    
-    def add(self, **kwargs):
-        usertype = kwargs.get('usertype', 'user')  # Default to 'user' if not provided
-        username = kwargs.get('username')
-        firstname = kwargs.get('firstname')
-        middlename = kwargs.get('middlename')
-        lastname = kwargs.get('lastname')
-        password = kwargs.get('password')
-        dateofbirth = kwargs.get('dateofbirth')
-        phone = kwargs.get('phone')
-        email = kwargs.get('email')
-        address = kwargs.get('address')
-        db_path = kwargs.get('db_path', DB_PATH)
-        
-        conn = sqlite3.connect(db_path)
-        cursor = conn.cursor()
-        cursor.execute("""
-            INSERT INTO users (usertype, username, firstname, middlename, lastname, password, dateofbirth, phone, email, address)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-        """, ( usertype,username, firstname, middlename, lastname, password, dateofbirth, phone, email, address))
-        conn.commit()
-        conn.close()
-
-    def update(self, **kwargs):
-        user_id = kwargs.get('user_id')
-        usertype = kwargs.get('usertype', 'user')  # Default to 'user' if not provided
-        username = kwargs.get('username')
-        firstname = kwargs.get('firstname')
-        middlename = kwargs.get('middlename')
-        lastname = kwargs.get('lastname')
-        password = kwargs.get('password')
-        dateofbirth = kwargs.get('dateofbirth')
-        phone = kwargs.get('phone')
-        email = kwargs.get('email')
-        address = kwargs.get('address')
-        db_path = kwargs.get('db_path', DB_PATH)
-        
-        conn = sqlite3.connect(db_path)
-        cursor = conn.cursor()
-        cursor.execute("""
-            UPDATE users
-            SET usertype=?, username = ?, firstname = ?, middlename = ?, lastname = ?, password = ?, dateofbirth = ?, phone = ?, email = ?, address = ?
-            WHERE user_id = ?
-        """, (usertype, username, firstname, middlename, lastname, password, dateofbirth, phone, email, address, user_id))
-        conn.commit()
-        conn.close()
-    
-    def get_all(self, db_path=DB_PATH):
-        conn = sqlite3.connect(db_path)
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM users")
-        results = cursor.fetchall()
-        conn.close()
-        return results
-     
-class Policy(Database):
-    def __init__(self, db_path=DB_PATH):
-        conn = sqlite3.connect(db_path)
-        cursor = conn.cursor()
-        create_query = """
-        CREATE TABLE IF NOT EXISTS policies (
-            policy_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            policy_number TEXT UNIQUE NOT NULL,
-            policy_holder TEXT NOT NULL,
-            user_id INTEGER,
-            start_date TEXT NOT NULL,
-            end_date TEXT NOT NULL,
-            premium REAL NOT NULL,
-            status TEXT CHECK(status IN ('active', 'inactive')) NOT NULL,
-            FOREIGN KEY(user_id) REFERENCES users(user_id)
-        )
-        """
-        cursor.execute(create_query)
-        conn.commit()
-        conn.close()
-
-    def get_id(self, db_path=DB_PATH):
-        conn = sqlite3.connect(db_path)
-        cursor = conn.cursor()
-        cursor.execute("SELECT MAX(policy_id) FROM policies")
-        result = cursor.fetchone()[0]
-        conn.close()
-        return result if result is not None else 0
-
-    def generate_policy_number(self, db_path=DB_PATH):
-        today_str = datetime.now().strftime("%Y%m%d")
-        conn = sqlite3.connect(db_path)
-        cursor = conn.cursor()
-        cursor.execute(
-            "SELECT COUNT(*) FROM policies WHERE policy_number LIKE ?",
-            (f"PLC-{today_str}%",)
-        )
-        count = cursor.fetchone()[0] + 1
-        conn.close()
-        return f"PLC-{today_str}{count:02d}"
-    
-    def add(self, **kwargs):
-        policy_holder = kwargs.get('policy_holder')
-        start_date = kwargs.get('start_date')
-        end_date = kwargs.get('end_date')
-        premium = kwargs.get('premium')
-        status = kwargs.get('status')
-        user_id = kwargs.get('user_id')  # Add this line
-        db_path = kwargs.get('db_path', DB_PATH)
-        policy_number = self.generate_policy_number(db_path)
-        conn = sqlite3.connect(db_path)
-        cursor = conn.cursor()
-        cursor.execute("""
-            INSERT INTO policies (policy_number, policy_holder, user_id, start_date, end_date, premium, status)
-            VALUES (?, ?, ?, ?, ?, ?, ?)
-        """, (policy_number, policy_holder, user_id, start_date, end_date, premium, status))
-        conn.commit()
-        policy_id = cursor.lastrowid
-        conn.close()
-
-    def get_by_id(self, id, db_path=DB_PATH):
-        conn = sqlite3.connect(db_path)
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM policies WHERE policy_id = ?", (id,))
-        result = cursor.fetchone()
-        conn.close()
-        return result
-
-    def update(self, **kwargs):
-        policy_id = kwargs.get('policy_id')
-        policy_holder = kwargs.get('policy_holder')
-        start_date = kwargs.get('start_date')
-        end_date = kwargs.get('end_date')
-        premium = kwargs.get('premium')
-        status = kwargs.get('status')
-        db_path = kwargs.get('db_path', DB_PATH)
-        
-        conn = sqlite3.connect(db_path)
-        cursor = conn.cursor()
-        cursor.execute("""
-            UPDATE policies
-            SET policy_holder = ?, start_date = ?, end_date = ?, premium = ?, status = ?
-            WHERE policy_id = ?
-        """, (policy_holder, start_date, end_date, premium, status, policy_id))
-        conn.commit()
-        conn.close()
-
-    def get_all(self, db_path=DB_PATH):
-        conn = sqlite3.connect(db_path)
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM policies")
-        results = cursor.fetchone()
-        conn.close()
-        return results
-
-class Vehicle(Database):
-    def __init__(self, db_path=DB_PATH):
-        conn = sqlite3.connect(db_path)
-        cursor = conn.cursor()
-        create_query = """
-        CREATE TABLE IF NOT EXISTS vehicles (
-            vehicle_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            policy_id INTEGER NOT NULL,
-            typeofvehicle TEXT CHECK(typeofvehicle IN ('fourwheeler', 'threewheeler', 'twowheeler', 'other')) NOT NULL,
-            image_path TEXT,
-            make TEXT NOT NULL,
-            model TEXT NOT NULL,
-            year_of_purchase INTEGER NOT NULL,
-            damage_report TEXT,
-            FOREIGN KEY(policy_id) REFERENCES policies(policy_id)
-        )
-        """
-        cursor.execute(create_query)
-        conn.commit()
-        conn.close()
-
-    def add(self, **kwargs):
-        policy_id = kwargs.get('policy_id')
-        typeofvehicle = kwargs.get('typeofvehicle', 'other')
-        image_path = kwargs.get('image_path')
-        make = kwargs.get('make')
-        model = kwargs.get('model')
-        year_of_purchase = kwargs.get('year_of_purchase')
-        db_path = kwargs.get('db_path', DB_PATH)
-        
-        conn = sqlite3.connect(db_path)
-        cursor = conn.cursor()
-        cursor.execute("""
-            INSERT INTO vehicles (policy_id,typeofvehicle, image_path, make, model, year_of_purchase)
-            VALUES (?, ?, ?, ?, ?, ?)
-        """, (policy_id,typeofvehicle, image_path, make, model, year_of_purchase))
-        conn.commit()
-        conn.close()
-
-    def get_by_id(self, id, db_path=DB_PATH):
-        conn = sqlite3.connect(db_path)
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM vehicles WHERE vehicle_id = ?", (id,))
-        result = cursor.fetchone()
-        conn.close()
-        return result
-
-    def update(self, **kwargs):
-        vehicle_id = kwargs.get('vehicle_id')
-        policy_id = kwargs.get('policy_id')
-        typeofvehicle = kwargs.get('typeofvehicle', 'other')
-        image_path = kwargs.get('image_path')
-        make = kwargs.get('make')
-        model = kwargs.get('model')
-        year_of_purchase = kwargs.get('year_of_purchase')
-        db_path = kwargs.get('db_path', DB_PATH)
-        
-        conn = sqlite3.connect(db_path)
-        cursor = conn.cursor()
-        cursor.execute("""
-            UPDATE vehicles
-            SET policy_id = ?, typeofvehicle=?, image_path = ?, make = ?, model = ?, year_of_purchase = ?
-            WHERE vehicle_id = ?
-        """, (policy_id, typeofvehicle, image_path, make, model, year_of_purchase, vehicle_id))
-        conn.commit()
-        conn.close()
-
-    def get_all(self, db_path=DB_PATH):
-        conn = sqlite3.connect(db_path)
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM vehicles")
-        results = cursor.fetchall()
-        conn.close()
-        return results
-    
-class Claims(Database):
     pass
+     
+class policy_methods():
+    def generate_policy_number(self, db):
+        today_str = datetime.now().strftime("%Y%m%d")
+        count = db.query(Policy).count()
+        return f"POL-{today_str}{count:02d}"
+    
+        
+
+class vehicle_methods():
+    pass
+    
+class claims_methods():
+    
+    def generate_claim_number(self, db ):
+        today_str = datetime.now().strftime("%Y%m%d")
+        count = db.query(Claims).count()
+        return f"CLM-{today_str}{count:02d}"
 
 
 # Call this function once at the start of your app
 if __name__ == "__main__":
-    pass
+    
+    db = sessionlocal()
+    print(claims_methods().generate_claim_number(db))
+    # User().initialize_DB()
