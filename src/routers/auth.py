d66912c7ce08d1eb23e474e6d4c74a90bcc741ef
Hari vardhana shubhash <Hari.Ponnamanda@valuemomentum.com>
2025-08-20 17:40:31 +0530

    improved authentication and authorization

--- src/routers/auth.py
+++ src/routers/auth.py
@@ -1,14 +1,28 @@
-from fastapi import APIRouter, Depends # type: ignore
-from fastapi.security import OAuth2PasswordRequestForm# type: ignore
+from datetime import timedelta, datetime, timezone
+from fastapi import APIRouter, Depends, HTTPException, status, Response # type: ignore
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer #type: ignore
+from pydantic import BaseModel# type: ignore
 from database.database import db_dependency
 from database.model import User
 
 from typing import Annotated
 import requests
-from passlib.context import CryptContext
+from json import load
+from passlib.context import CryptContext #type: ignore
+from jose import jwt #type: ignore
+from jose.exceptions import JWTError #type: ignore
 
 router = APIRouter(prefix="/auth", tags=["authentication"])
+
+SECRET_KEY = '4b6850b91f5ba7c307b461150fb713fa84e134791539c52cba3653b29290db52'
+ALGORITHM = 'HS256'
+
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl = 'auth/token')
+
+class Token(BaseModel):
+    access_token:str
+    token_type:str
 
 def authenticate_user(db, username: str, password: str):
     user= db.query(User).filter(User.username == username).first()
@@ -16,10 +30,51 @@ def authenticate_user(db, username: str, password: str):
         return False
     if not bcrypt_context.verify(password, user.hashed_password):
         return False
-    return True
+    return user
 
-@router.post("/token")
-async def auth(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
-    if not authenticate_user(db , form_data.username, form_data.password):
-        return {"error": "Invalid credentials"}
-    return db.query(User).filter(User.username==form_data.username).first()
+def create_access_token(username:str, user_id: int, role:str, expires_delta: timedelta):
+    encode = {'sub': username, 'user_id': user_id, 'role': role}
+    expires = datetime.now(timezone.utc) + expires_delta
+    encode.update({'exp': expires})
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
+    if token is None:
+        # Try reading from cookies
+        token = request.cookies.get("access_token_fnol")
+    if token is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Not authenticated")
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username = payload.get('sub')
+        user_id = payload.get('user_id')
+        role = payload.get('role')
+        if username is None or user_id is None or role is None:
+            raise HTTPException(status_code = status.HTTP_401_UNAUTHORIZED,
+                                detail='Could not validete user')
+        return {'username': username, 'user_id': user_id, 'role': role }
+    except JWTError:
+        raise HTTPException(status_code = status.HTTP_401_UNAUTHORIZED,
+                                detail='Could not validete user')
+
+@router.post("/token", response_model=Token)
+async def auth(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency, response: Response):
+    user = authenticate_user(db , form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid credentials"
+        )
+    token = create_access_token(user.username, user.user_id, user.usertype, timedelta(minutes=30)) #type: ignore
+    response.set_cookie(
+        key="access_token_fnol",
+        value=token,
+        httponly=True,       # prevents JavaScript access (safer)
+        max_age=1800,        # 30 minutes
+        expires=1800,
+        secure=True,         # use HTTPS in production
+        samesite="Lax"
+    )
+
+    return {'access_token': token, 'token_type': 'bearer'}
